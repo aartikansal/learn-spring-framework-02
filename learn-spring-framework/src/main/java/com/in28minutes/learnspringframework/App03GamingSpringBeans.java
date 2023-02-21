c0c958788315079b676bd2c3a209dcd1df33e4da
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-21 18:14:30 -0500

    Config file into main @ComponentScan added

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -5,21 +5,20 @@ import com.in28minutes.learnspringframework.game.GamingConsole;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.Annotation;
 
 @Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
 
 public class App03GamingSpringBeans {
-    @Bean
-    public GamingConsole game() {
-        var game = new PacmanGame();//Object creation
-        return game;
-    }
+
 
     @Bean
     public GameRunner gameRunner(GamingConsole game) {
+        System.out.println("Parameter: " + game);
         var gameRunner = new GameRunner(game);
         return gameRunner;
     }
