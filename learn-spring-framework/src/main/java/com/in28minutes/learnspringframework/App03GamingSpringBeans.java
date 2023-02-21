331f10f00df8dfd1d5e814b6d6f0f46d96966288
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-21 18:08:39 -0500

    Initial commit

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -0,0 +1,47 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.Annotation;
+
+@Configuration
+
+public class App03GamingSpringBeans {
+    @Bean
+    public GamingConsole game() {
+        var game = new PacmanGame();//Object creation
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+    }
+}
+
+//var game= new MarioGame(); //1.Object creation
+//var game= new SuperContraGame();
+//var game = new PacmanGame();
+//var gameRunner = new GameRunner(game);
+//2: Object creation + Wiring of Dependencies
+//passing game parameter to the constructor of Game Runner
+//Game is a dependency of GameRunner class
+// gameRunner.run();
+
+
+
+
+
